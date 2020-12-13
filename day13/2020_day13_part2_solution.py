701e73d5772efaaaab84be68f6ef247b3a87030c
fcspiteri <fcspiteri@gmail.com>
2020-12-13 08:16:49 -0800

    Add day 13 part 2 solution

--- day13/2020_day13_part2_solution.py
+++ day13/2020_day13_part2_solution.py
@@ -9,16 +9,17 @@ for i in range(0, len(bus_list)):
     if bus_list[i] != 'x':
         in_service.append([i, int(bus_list[i])])
     
-new_time = 700000000000000
+new_time = 500000000000000
 run = True
 while run == True:
-    if new_time % in_service[0][1] == 0 and (new_time+in_service[-1][0])%in_service[-1][1] == 0:
-        for bus in in_service:
+    for bus in in_service:
             if (new_time + bus[0]) % bus[1] == 0:
                 if bus[0] == in_service[-1][0]:
                     print('FOUND', new_time)
                     run = False
             else:
+                b = bus[1]
+                t = new_time + bus[0]
+                print(new_time, new_time + b-t%b)
+                new_time = new_time + b - t%b
                 break
-    new_time += 2
-    print (new_time)
