efd46fdbad1d922ed240902cd3e430afed16e472
fcspiteri <fcspiteri@gmail.com>
2020-12-13 09:23:26 -0800

    Add day 13 part 2 solution

--- day13/2020_day13_part2_solution.py
+++ day13/2020_day13_part2_solution.py
@@ -1,3 +1,5 @@
+import math
+
 with open('input13.txt','r') as f_open:
     data = f_open.read()
 data = data.split('\n')
@@ -5,21 +7,28 @@ data = data.split('\n')
 earliest_time = int(data[0])
 bus_list = data[1].split(',')
 in_service = []
+max_bus = 0
+max_bus_pos = 0
 for i in range(0, len(bus_list)):
     if bus_list[i] != 'x':
         in_service.append([i, int(bus_list[i])])
-    
-new_time = 500000000000000
+        if int(bus_list[i]) > max_bus:
+            max_bus = int(bus_list[i])
+            max_bus_pos = i
+print(in_service)
+
+new_time = 50000000000000
 run = True
 while run == True:
+    print(new_time)
     for bus in in_service:
+        if (new_time+max_bus_pos) % max_bus == 0:
             if (new_time + bus[0]) % bus[1] == 0:
                 if bus[0] == in_service[-1][0]:
                     print('FOUND', new_time)
                     run = False
             else:
-                b = bus[1]
-                t = new_time + bus[0]
-                print(new_time, new_time + b-t%b)
-                new_time = new_time + b - t%b
+                new_time += max_bus
                 break
+        else:
+            new_time += 1
